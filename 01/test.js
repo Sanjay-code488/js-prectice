abed562c3cdcdc4150f561408e14514e601ad489
Sanjay-code488 <sanjaybpwebsolution@gmail.com>
2024-06-13 11:18:08 +0000

    variable

--- 01/test.js
+++ 01/test.js
@@ -1,1 +1,13 @@
-console.log("Sanjay")
+
+const accountID = 144553
+let accountEmail = "test@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+accountEmail = "t@gmail.com"
+accountPassword = "678910"
+accountCity = "Chomu"
+
+console .table([accountId, accountEmail, accountPassword, accountCity ])
+
